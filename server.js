4cbb07c47e2073832052e026fbafc8f5fec45e67
Faycal88 <99132896+Faycal88@users.noreply.github.com>
2022-08-14 19:50:10 +0100

    adding sitemap middleware

--- server.js
+++ server.js
@@ -31,7 +31,21 @@ const OrderRouter = require("./routes/orders");
 const CollectionRouter = require("./routes/collections");
 const CategoryRouter = require("./routes/categories");
 const NewsletterRouter = require("./routes/newsletter");
-const SitemapProvider = require("./routes/sitemap");
+
+const expressSitemapXml = require("express-sitemap-xml");
+
+app.use(expressSitemapXml(getUrls, "https://eternelle-plantes.store/"));
+app.use(expressSitemapXml(getUrls, "https://eternelle-plantes.store/login"));
+app.use(expressSitemapXml(getUrls, "https://eternelle-plantes.store/register"));
+app.use(expressSitemapXml(getUrls, "https://eternelle-plantes.store/shop"));
+app.use(expressSitemapXml(getUrls, "https://eternelle-plantes.store/stores"));
+app.use(
+  expressSitemapXml(getUrls, "https://eternelle-plantes.store/product/:id")
+);
+
+async function getUrls() {
+  return await getUrlsFromDatabase();
+}
 
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
@@ -46,7 +60,6 @@ app.use("/orders", OrderRouter);
 app.use("/collections", CollectionRouter);
 app.use("/categories", CategoryRouter);
 app.use("/subscribers", NewsletterRouter);
-app.use("/sitemap", SitemapProvider);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
